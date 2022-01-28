54366635d224ad15e560b5a5b79050396e434afd
Daniel Huber <daniel.huber@bullhorn.com>
2022-01-28 00:11:57 -0500

    Init

--- src/card-display/CardDisplay.js
+++ src/card-display/CardDisplay.js
@@ -1,36 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
-import './card-display/CardDisplay.js';
-import './roll-counter/RollCounter.js';
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import './CardDisplay.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class CardDisplay extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cardList: [],
+            shuffledCardList: [],
+            shownCards: []
+        };
+        this.handleClick = this.handleClick.bind(this);
+        this.makeDecks();
+    }
 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    importAll(r) {
+        let images = {};
+        r.keys().map((item, index) => { images[item.replace('../img/cards', '')] = r(item); });
+        return images;
+    }
+
+    shuffleDeck(deck) {
+        console.log('shuffleDeck');
+        for (var i = deck.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = deck[i];
+            deck[i] = deck[j];
+            deck[j] = temp;
+        }
+    }
+
+    handleClick() {
+        console.log('handleClick');
+        const discardPile = this.state.shownCards;
+        const shuffledCardList = this.state.shuffledCardList;
+        discardPile.push(shuffledCardList.pop());
+
+        this.setState({
+            shownCards: discardPile
+        });
+    }
+
+    makeDecks() {
+        console.log('making decks');
+        const images = this.importAll(require.context('../img/cards', false, /\.(png|jpe?g|svg)$/));
+        const cardImages = [];
+
+        Object.keys(images).forEach(key => {
+            cardImages.push(images[key]);
+        });
+
+        const tempDeck = cardImages;
+        const shownCards = this.state.shownCards;
+        shownCards.push(tempDeck.pop());
+        this.shuffleDeck(tempDeck);
+
+        this.state.cardList = cardImages;
+        this.state.shuffledCardList = tempDeck;
+        // this.setState({
+        //     cardList: cardImages,
+        //     shuffledCardList: tempDeck
+        // });
+    }
+
+    render() {
+        console.log('rendering card');
+        const cardToShow = this.state.shownCards[this.state.shownCards.length - 1];
+
+        return (
+            <div className="Card">
+                {/*<img src={this.state.shownCards}*/}
+                     {/*className="card-img"*/}
+                     {/*alt="pic"*/}
+                     {/*onClick={this.handleClick()}*/}
+                {/*/>*/}
+                <img key={Date.now()}
+                     src={cardToShow}
+                     className="card-img"
+                     alt="pic"
+                     onClick={this.handleClick}
+                />
+            </div>
+        );
+    }
 }
 
-function CardDisplay() {
-
-}
-
-function RollCounter() {
-
-}
-
-export default App;
+export default CardDisplay;
--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -1,6 +1,65 @@
 import React, {Component} from 'react'
+import pw from '../img/symbols/PW.png'
+import chaos from '../img/symbols/CHAOS.png'
+import whitespace from '../img/symbols/whitespace.png'
 import ReactDOM from 'react-dom'
+import './Controls.css'
 
-class RollCounter extends Component {
+class Controls extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentResult: whitespace,
+            count: 0
+        };
+        this.handleRoll = this.handleRoll.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+    }
 
+    handleRoll() {
+        console.log('handleRoll');
+        const options = [pw, chaos, whitespace, whitespace, whitespace, whitespace];
+        const randomIdx = Math.floor(Math.random() * options.length);
+        const newResult = options[randomIdx];
+        const newCount = this.state.count + 1;
+
+        this.setState({
+            currentResult: newResult,
+            count: newCount
+        });
+    }
+
+    handleReset() {
+        console.log('handleReset');
+        this.setState({
+            count: 0
+        });
+    }
+
+    render() {
+        console.log('rendering buttons');
+        const result = this.state.currentResult;
+        const count = this.state.count;
+
+        return (
+            <div className="Controls">
+                <div className="Buttons">
+                    <button className="Roll" onClick={this.handleRoll}>Roll</button>
+                    <button className="Reset" onClick={this.handleReset}>Reset</button>
+                </div>
+                <div className="Display">
+                    <img key={Date.now()}
+                         src={result}
+                         className="result-img"
+                    />
+                    <p>
+                        <h2>{count}</h2>
+                    </p>
+                </div>
+
+            </div>
+        );
+    }
 }
+
+export default Controls;
